11de24d940854f7d66dfdcd088471071c41d4b65
nsivaji61 <33428330+nsivaji61@users.noreply.github.com>
2019-01-25 16:21:39 -0500

    Added new api project and aws cloud formation templates for micro service

---
+++ Src/Products/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] { "Internet", "Voice" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
